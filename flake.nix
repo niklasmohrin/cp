4ab74774efb12aa9aa31fd28eef22aea838fa7af
Niklas Mohrin <dev@niklasmohrin.de>
2023-11-23 00:58:19 +0100

    Build pdfs with nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,53 @@
 {
   description = "NACTL";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    kactl = {
+      url = "github:kth-competitive-programming/kactl";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
-      {
+      rec {
+        packages = rec {
+          default = nactl;
+
+          nactl = pkgs.stdenvNoCC.mkDerivation {
+            name = "nactl";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.typst ];
+            phases = [ "unpackPhase" "buildPhase" ];
+            buildPhase = "typst compile nactl.typ $out";
+          };
+
+          kactl = pkgs.stdenvNoCC.mkDerivation {
+            name = "kactl";
+            src = inputs.kactl;
+            phases = [ "unpackPhase" "patchPhase" "buildPhase" "installPhase" ];
+            nativeBuildInputs = with pkgs; [
+              python310
+              (texlive.combine {
+                inherit (texlive) scheme-medium enumitem titling todonotes cleveref framed tocloft titlesec paralist;
+              })
+            ];
+            patches = [ ./kactl.patch ];
+            buildPhase = "make kactl";
+            installPhase = "cp kactl.pdf $out";
+          };
+
+          nactl-complete = pkgs.runCommand "nactl-complete" { } "${pkgs.poppler_utils}/bin/pdfunite ${nactl} ${kactl} $out";
+        };
+
         devShells.default = pkgs.mkShell {
+          inputsFrom = with packages; [ nactl ];
           packages = with pkgs; [
-            typst
             typst-lsp
             typst-fmt
 
